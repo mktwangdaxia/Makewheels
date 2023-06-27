dba6221d553d9424edd191d295aec208989873dc
mktwangdaxia <2268481310>
2023-06-27 14:08:01 +0800

    提交了

--- gulu-ui-1/src/views/Home.vue
+++ gulu-ui-1/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template><div><div class="topnav">
-    <div class="logo"></div> 
+    <div class="logo">LOGO</div> 
     <ul class="menu">
         <li>菜单1</li>
         <li>菜单2</li>
@@ -9,11 +9,17 @@
         <h1>轱辘UI</h1>
         <h2>一个厉害的UI框架</h2>
         <p class="actions">
-            <a>GitHub</a>
-            <a>开始</a>
+            <a href="https://github.com">GitHub</a>
+            <router-link to="/doc">开始</router-link>
         </p>
     </div>
 </template>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default{
+    components:{Topnav}
+}
+</script>
 <style lang="scss" scoped>
 .topnav{
     background-color: rgba(98, 38, 150, 0.741);
